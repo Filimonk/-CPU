6b6b90e9bd2b54f0e9ce76efa21cac2280e3aa6a
Filimonk <61885735+Filimonk@users.noreply.github.com>
2024-03-01 15:09:08 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -8,29 +8,39 @@ signed main() {
     b.push(2);
     b.push(3);
     
-    stack::Stack <int> c = b;
+    stack::Stack <int> c(b);
      
+    std::cout << "c:\n";
     while (c.getSize()) {
         std::cout << c.top() << " ";
         c.pop();
     }
-    
     std::cout << "\n";
+    
     c.push(4);
+    c.push(5);
+    c.push(6);
+    
     stack::Stack <int> d = c;
     
+    d.pop();
+    d.push(7);
+    
+    std::cout << "b:\n";
     while (b.getSize()) {
         std::cout << b.top() << " ";
         b.pop();
     }
     std::cout << "\n";
      
+    std::cout << "c:\n";
     while (c.getSize()) {
         std::cout << c.top() << " ";
         c.pop();
     }
     std::cout << "\n";
      
+    std::cout << "d:\n";
     while (d.getSize()) {
         std::cout << d.top() << " ";
         d.pop();
